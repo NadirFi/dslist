6d01704450ba3f90e8d9f88d667df9b69c222efc
nadirfilho <nadirssfilho@gmail.com>
2024-11-11 14:33:03 -0300

    Game seed

--- src/main/java/com/devsuperior/dslist/entities/Games.java
+++ src/main/java/com/devsuperior/dslist/entities/Games.java
@@ -12,22 +12,29 @@ public class Games {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
     private String title;
+
     @Column(name = "game_year")
     private Integer year;
     private String genre;
-    private String platform;
+    private String platforms;
+    private Double score;
     private String imgUrl;
+
+    @Column(columnDefinition = "TEXT")
     private String shortDescription;
+    
+    @Column(columnDefinition = "TEXT")
     private String longDescription;
 
     public Games() {}
 
-    public Games(long id, String title, Integer year, String genre, String platform, String imgUrl, String shortDescription, String longDescription) {
+    public Games(long id, String title, Integer year, String genre, String platforms, Double score, String imgUrl, String shortDescription, String longDescription) {
         this.id = id;
         this.title = title;
         this.year = year;
         this.genre = genre;
-        this.platform = platform;
+        this.platforms = platforms;
+        this.score = score;
         this.imgUrl = imgUrl;
         this.shortDescription = shortDescription;
         this.longDescription = longDescription;
@@ -73,12 +80,20 @@ public class Games {
         this.imgUrl = imgUrl;
     }
 
-    public String getPlatform() {
-        return platform;
+    public String getPlatforms() {
+        return platforms;
     }
 
-    public void setPlatform(String platform) {
-        this.platform = platform;
+    public void setPlatforms(String platform) {
+        this.platforms = platform;
+    }
+
+    public Double getScore() {
+        return score;
+    }
+
+    public void setScore(Double score) {
+        this.score = score;
     }
 
     public String getShortDescription() {
