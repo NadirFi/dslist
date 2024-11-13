f8512e500d400e57bb18bea183abc2dd2bacf548
nadirfilho <nadirssfilho@gmail.com>
2024-11-13 13:59:18 -0300

    GET game by id

---
+++ src/main/java/com/devsuperior/dslist/dto/GameFullDTO.java
@@ -0,0 +1,73 @@
+package com.devsuperior.dslist.dto;
+
+import com.devsuperior.dslist.entities.Game;
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import org.springframework.beans.BeanUtils;
+
+public class GameFullDTO {
+
+    private long id;
+    private String title;
+    private Integer year;
+    private String genre;
+    private String platforms;
+    private Double score;
+    private String imgUrl;
+    private String shortDescription;
+    private String longDescription;
+
+    public GameFullDTO() {}
+
+    public GameFullDTO(Game game) {
+        //BeanUtils.copyProperties(game, this);
+
+        this.id = game.getId();
+        this.title = game.getTitle();
+        this.year = game.getYear();
+        this.genre = game.getGenre();
+        this.platforms = game.getPlatforms();
+        this.score = game.getScore();
+        this.imgUrl = game.getImgUrl();
+        this.shortDescription = game.getShortDescription();
+        this.longDescription = game.getLongDescription();
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public Integer getYear() {
+        return year;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public String getPlatforms() {
+        return platforms;
+    }
+
+    public Double getScore() {
+        return score;
+    }
+
+    public String getImgUrl() {
+        return imgUrl;
+    }
+
+    public String getShortDescription() {
+        return shortDescription;
+    }
+
+    public String getLongDescription() {
+        return longDescription;
+    }
+}
--- src/main/java/com/devsuperior/dslist/services/GameService.java
+++ src/main/java/com/devsuperior/dslist/services/GameService.java
@@ -1,10 +1,12 @@
 package com.devsuperior.dslist.services;
 
 import com.devsuperior.dslist.dto.GameDTO;
+import com.devsuperior.dslist.dto.GameFullDTO;
 import com.devsuperior.dslist.entities.Game;
 import com.devsuperior.dslist.repositories.GameRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -14,6 +16,13 @@ public class GameService {
     @Autowired
     private GameRepository gameRepository;
 
+    @Transactional(readOnly = true)
+    public GameFullDTO findById(Long id) {
+        Game result = gameRepository.findById(id).get();
+        return  new GameFullDTO(result);
+    }
+
+    @Transactional(readOnly = true)
     public List<GameDTO> findAll() {
         List<Game> result = gameRepository.findAll();
         return result.stream().map(x -> new GameDTO(x)).toList();
